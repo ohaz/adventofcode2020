5e31e4ca4ed0da7e5a24b7b6c04e5ffe657f332f
Richard Baumann <ohaz@ohaz.engineer>
2020-12-02 08:07:43 +0100

    Fix day display

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from day2 import day2
 days = [(day1.sub1, day1.sub2), (day2.sub1, day2.sub2)]
 
 for index, day in enumerate(days):
-    print(f'* Running day {index}')
+    print(f'* Running day {index + 1}')
     print('- Running part 1')
     day[0]()
     print('- Running part 2')

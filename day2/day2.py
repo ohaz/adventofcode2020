6b004a2af5c59aa694439b641f939dbbaf1cb6f5
Richard Baumann <ohaz@ohaz.engineer>
2020-12-02 08:27:17 +0100

    Add solution for day2

--- day2/day2.py
+++ day2/day2.py
@@ -1,5 +1,34 @@
+import re
+from file_reader import get_file_entries
+
+regex = re.compile(r'(?P<min>\d+)-(?P<max>\d+)\s(?P<policy>\w):\s(?P<password>.*)')
+
+def is_valid_sub1_password(line):
+    parsed = regex.match(line)
+    amount = parsed.group('password').count(parsed.group('policy'))
+    return int(parsed.group('min')) <= amount <= int(parsed.group('max'))
+
 def sub1():
-    pass
+    lines = get_file_entries('./day2/input.txt')
+    amount = 0
+    for line in lines:
+       amount += is_valid_sub1_password(line)
+    print(amount)
+
+def is_valid_sub2_password(line):
+    parsed = regex.match(line)
+    position1 = int(parsed.group('min')) - 1
+    position2 = int(parsed.group('max')) - 1
+    password = parsed.group('password')
+    policy = parsed.group('policy')
+    amount = 0
+    amount = amount + 1 if password[position1] == policy else amount
+    amount = amount + 1 if password[position2] == policy else amount
+    return amount == 1
 
 def sub2():
-    pass
+    lines = get_file_entries('./day2/input.txt')
+    amount = 0
+    for line in lines:
+        amount += is_valid_sub2_password(line)
+    print(amount)
